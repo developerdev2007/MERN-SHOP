167e965c087f7356fafa6487125c803f5b6aa702
developerdev2007 <160243215+developerdev2007@users.noreply.github.com>
2024-08-15 00:23:07 +0530

    final commit

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,7 +7,7 @@ export const sendToken = async (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000), //90days
     httpOnly: true,
-    Secure: true,
+    secure: true,
     sameSite: "none",
   };
   res.status(statusCode).cookie("token", token, options).json({
--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -7,6 +7,8 @@ export const sendShopToken = async (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000), //90days
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
   };
   res.status(statusCode).cookie("seller_token", token, options).json({
     success: true,
